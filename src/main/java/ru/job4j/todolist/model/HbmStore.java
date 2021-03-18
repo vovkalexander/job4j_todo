9c700edfd6c157b9646c31905be3588fc426a1d5
Александр <46612532+vovkalexander@users.noreply.github.com>
2021-03-18 15:56:54 +0300

    Лямбды и шаблон wrapper.

--- src/main/java/ru/job4j/todolist/model/HbmStore.java
+++ src/main/java/ru/job4j/todolist/model/HbmStore.java
@@ -1,12 +1,13 @@
 package ru.job4j.todolist.model;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
+import java.util.function.Function;
 
 public class HbmStore implements Store, AutoCloseable {
     private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
@@ -24,60 +25,53 @@ public class HbmStore implements Store, AutoCloseable {
 
     @Override
     public Collection<Item> findAllItems() {
-        Collection<Item> result = new ArrayList();
-        try (Session session = sf.openSession()) {
-            session.beginTransaction();
-            result = session.createQuery("from ru.job4j.todolist.model.Item").list();
-            session.getTransaction().commit();
-            session.close();
-        } catch (Exception e) {
-            sf.getCurrentSession().getTransaction().rollback();
-        }
-        return result;
+        return this.tx(session ->
+                session.createQuery("from ru.job4j.todolist.model.Item order by id").list());
     }
 
     @Override
     public Item save(String text) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Item item = new Item();
-        item.setCreated(new Date());
-        item.setDescription(text);
-        session.save(item);
-        session.getTransaction().commit();
-        session.close();
-        return item;
+        return this.tx(session -> {
+            Item item = new Item();
+            item.setCreated(new Date());
+            item.setDescription(text);
+            session.save(item);
+            return item;
+        });
     }
 
     @Override
     public Item findItemById(int id) {
-        Item item = null;
-        try (Session session = sf.openSession()) {
-            session.beginTransaction();
-            item = (Item) session.byId(Item.class).load(id);
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            sf.getCurrentSession().getTransaction().rollback();
-        }
-        return item;
+        return this.tx(session -> session.byId(Item.class).load(id));
     }
 
     @Override
     public void replace(int id) {
         Item item = this.findItemById(id);
-        System.out.println(item);
         item.setDone(true);
-        try (Session session = sf.openSession()) {
-            session.beginTransaction();
+        this.tx(session -> {
             session.update(item);
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            sf.getCurrentSession().getTransaction().rollback();
-        }
+            return null;
+        });
     }
 
     @Override
     public void close() throws Exception {
         StandardServiceRegistryBuilder.destroy(registry);
+    }
+
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = sf.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
     }
 }
